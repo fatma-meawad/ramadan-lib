300c83ee1704a4dd7b7f3135ba60968805a421dd
Fatma <felsayedmeawad@gmail.com>
2025-03-09 18:04:35 +0100

    fixed the question redundancy

--- vars.js
+++ vars.js
@@ -1,36 +1,18 @@
 
 const data = [
-    {
-        "title": "تفسير الشعراوي : سورة الفاتحة",
-        "link": "",
-        "reference": "https://shamela.ws/book/1083/1",
-        "sections": [
-            {
-                "title": "بداية نزول القرآن الكريم",
-       "subsections": [
-            {
-                "title": "الحمد لله وعدل الله في الكون",
-                "link": "section3",
-                "keywords": ["",""],
-                "summary_file": "https://example.com/summary3.pdf"
-            }
-        ]}
-        ],
-        "image": "./images/Fatiha1.webp",
-        "questions": 72
-    },
+
     {
         "title": "تفسير الشعراوي : سورة البقرة ",
         "link": "https://app.quizwhizzer.com/play?code=14878",
         "reference": "https://shamela.ws/book/1083/28",
-        "sections":   [{
+        "sections": [{
             "title": "الأيات: 1 - 5",
-             "subsections": baqra1_sections
+            "subsections": baqra1_sections
         }],
         "image": "./images/Fatiha2.webp",
         "questions": "39"
     },
-    
+
     {
         "title": "العقيدة والإعجاز - الدرس تمهيد - علة وجود الإنسان 1",
         "link": "https://app.quizwhizzer.com/active/67c0b158c2b8ddf0858f98e1",
@@ -38,17 +20,38 @@ const data = [
         "sections": [
             {
                 "title": "بداية نزول القرآن الكريم",
-       "subsections": [
-            {
-                "title": "الحمد لله وعدل الله في الكون",
-                "link": "",
-                "keywords": ["",""],
-                "summary_file": "https://example.com/summary3.pdf"
+                "subsections": [
+                    {
+                        "title": "الحمد لله وعدل الله في الكون",
+                        "link": "",
+                        "keywords": ["", ""],
+                        "summary_file": "https://example.com/summary3.pdf"
+                    }
+                ]
             }
-        ]}
         ],
         "image": "./images/3akeedah.webp",
         "questions": "65"
+    },
+    {
+        "title": "تفسير الشعراوي : سورة الفاتحة",
+        "link": "",
+        "reference": "https://shamela.ws/book/1083/1",
+        "sections": [
+            {
+                "title": "بداية نزول القرآن الكريم",
+                "subsections": [
+                    {
+                        "title": "الحمد لله وعدل الله في الكون",
+                        "link": "section3",
+                        "keywords": ["", ""],
+                        "summary_file": "https://example.com/summary3.pdf"
+                    }
+                ]
+            }
+        ],
+        "image": "./images/Fatiha1.webp",
+        "questions": 72
     }
 ];
 
